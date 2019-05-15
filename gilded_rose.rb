b51c5c96de951f29339478fd2c08ded1bdc20d11
Amit Pandey <amit.pandey.iitm@gmail.com>
2019-05-15 11:16:34 +0530

    Add new item conjured

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -5,26 +5,16 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      next if item.quality < 1 || item.quality > 50 
       if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
+        item.quality = item.quality - 1 if item.quality > 0 && item.name != 'Sulfuras, Hand of Ragnaros'
+        item.quality = item.quality - 1 if item.quality > 0 && item.name == 'Conjured'
       else
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
+            item.quality = item.quality + 1 if item.sell_in < 11 && item.quality < 50
+            item.quality = item.quality + 1 if item.sell_in < 6 && item.quality < 50
           end
         end
       end
@@ -33,12 +23,10 @@ class GildedRose
       end
       if item.sell_in < 0
         if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
+          if item.name == 'Conjured' && item.quality > 1
+            item.quality = item.quality - 2
+          elsif item.name != 'Conjured' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros' && item.quality > 0
+            item.quality = item.quality - 1
           else
             item.quality = item.quality - item.quality
           end
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -5,7 +5,27 @@ describe GildedRose do
     it 'does not change the name' do
       items = [Item.new('foo', 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+      expect(items[0].name).to eq 'foo'
+    end
+    it 'does not make quality negative' do
+      items = [Item.new('Aged Brie', 3, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+    end
+    it 'does changes the quality' do
+      items = [Item.new('Aged Brie', 3, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 2
+    end
+    it 'decrease the quality by 2 for Conjured' do
+      items = [Item.new('Conjured', 3, 3)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 1
+    end
+    it 'decrease the quality by 4 for Conjured as sellin date passed' do
+      items = [Item.new('Conjured', -1, 4)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
     end
   end
 end
